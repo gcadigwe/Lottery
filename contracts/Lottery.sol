e942a27fe563ce0b800686f77732bfca6cc6f211
Sarzy <adigwegodswillchinonso@gmail.com>
2021-09-16 00:53:33 +0100

    setting up chainlink vrf for random number

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,18 +3,35 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery {
+contract Lottery is VRFConsumerBase,Ownable {
   address payable[] public players;
   uint256 public usdEntryFee;
   AggregatorV3Interface internal ethUsdPriceFeed;
+  enum LOTTERY_STATE{
+    OPEN,
+    CLOSED,
+    CALCULATING_WINNER
+  }
 
-  constructor(address _priceFeedAddress)public {
+  LOTTERY_STATE public lottery_state;
+
+  uint256 public fee;
+  bytes32 public keyhash;
+
+  constructor(address _priceFeedAddress,address _vrfCoordinator, address _link, uint256 _fee, bytes32 _keyhash)public VRFConsumerBase(_vrfCoordinator,_link) {
     usdEntryFee = 50 * (10**18);
     ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    lottery_state = LOTTERY_STATE.CLOSED;
+    fee = _fee;
+    keyhash = _keyhash;
   }
 
   function enter() public payable {
+    require(lottery_state == LOTTERY_STATE.OPEN);
+    require(msg.value >= getEntranceFee(),"Not enough ETH!");
     
     players.push(msg.sender);
   }
@@ -29,6 +46,21 @@ contract Lottery {
       return costToEnter;
 
   }
-  function startLottery() public {}
-  function endLottery() public {}
+  function startLottery() onlyOwner public {
+    require(lottery_state == LOTTERY_STATE.CLOSED,"Can't start a new lottery yet");
+
+    lottery_state = LOTTERY_STATE.OPEN;
+  }
+
+  function endLottery() onlyOwner public {
+    lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+    bytes32 requestId = requestRandomness(keyhash,fee);
+
+
+  }
+
+  function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
+    require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER,"You aren't there yet");
+    require(_randomness > 0,"random not found");
+  }
 }
